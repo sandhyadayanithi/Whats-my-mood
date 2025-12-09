b54191a565a82f78d3537ca978f5456a574b2df2
Sandhya <sandhyadayanithi@gmail.com>
2025-12-09 15:03:52 +0530

    Updated error handler

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ app.post('/analyse',async (req,res,next)=>{
 
 app.use((err,req,res,next)=>{
   if (err.status){
-    res.status(err,status).json({msg: err.message});
+    res.status(err.status).json({msg: err.message});
   }
   else{
     res.status(500).json({msg: err.message});
